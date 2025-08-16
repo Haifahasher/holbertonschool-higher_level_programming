91d9f62028d873b1403167c1601693034b56ab96
Haifahasher <Haifahasher@gmail.com>
2025-08-16 08:50:24 +0000

    fix 10 and 11

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,38 +1,17 @@
 #!/usr/bin/python3
 """
-Module defining a Student class with filtered JSON representation
+Class student module
 """
 
 
 class Student:
-    """
-    A class that defines a student
-    """
-
     def __init__(self, first_name, last_name, age):
-        """
-        Initialize a Student instance
-
-        Args:
-            first_name (str): The student's first name
-            last_name (str): The student's last name
-            age (int): The student's age
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Retrieves a dictionary representation of a Student instance
-
-        Args:
-            attrs (list): A list of strings representing attribute names to include
-
-        Returns:
-            dict: A dictionary representation of the Student instance
-        """
-        if attrs is None:
-            return self.__dict__
-        else:
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
+        return {
+                k: v for k, v in self.__dict__.items()
+                if attrs is None or k in attrs
+                }
