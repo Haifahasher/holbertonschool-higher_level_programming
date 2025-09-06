8c02e896896942fa6603c62fc8ddeb5e012c0bd6
Haifahasher <Haifahasher@gmail.com>
2025-09-06 20:43:28 +0000

    update 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,45 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""
+Lists all states from the database hbtn_0e_0_usa.
+And order it using ascending order
+"""
+
+import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
+    # Get MySQL credentials and database name from arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    import MySQLdb
-    import sys
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-    )
+        # Create a cursor to execute queries
+        cursor = db.cursor()
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+        # Execute SQL query to select all states sorted by id
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+        # Fetch all rows
+        rows = cursor.fetchall()
+
+        # Print each row
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print(f"Error connecting to databse, {e}")
+    finally:
+        # Clean up
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
